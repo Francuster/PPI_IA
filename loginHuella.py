f04989c87c73b5baf38d7aae83dd81e95eb6b8e0
diegoayza <diegoayza82@gmail.com>
2024-03-28 09:53:55 -0300

    agregando imagenes de prueba

---
+++ loginHuella.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+
+# Base de datos de imágenes de huellas digitales (simulada)
+database = {
+    'usuario1': cv2.imread('img/huella1.jpg', cv2.IMREAD_GRAYSCALE),
+    'usuario2': cv2.imread('img/huella2.jpg', cv2.IMREAD_GRAYSCALE),
+    'usuario3': cv2.imread('img/huella3.jpg', cv2.IMREAD_GRAYSCALE),
+
+}
+
+def compare_fingerprints(fingerprint1, fingerprint2):
+    # Calculamos la diferencia absoluta entre las dos imágenes
+    difference = cv2.absdiff(fingerprint1, fingerprint2)
+    return np.sum(difference)
+
+def authenticate_fingerprint(input_fingerprint):
+    min_difference = 150000 #float('inf')
+    authenticated_user = None
+
+    for username, stored_fingerprint in database.items():
+        difference = compare_fingerprints(input_fingerprint, stored_fingerprint)
+        
+        if difference <= min_difference:
+            min_difference = difference
+            authenticated_user = username
+
+    return authenticated_user
+
+# Simulación de captura de huella digital (cargando una imagen)
+input_fingerprint = cv2.imread('img/huella_a_comparar.jpg', cv2.IMREAD_GRAYSCALE)
+
+# Autenticación de la huella digital capturada
+authenticated_user = authenticate_fingerprint(input_fingerprint)
+
+if authenticated_user:
+    print(f'Autenticación exitosa. Bienvenido, {authenticated_user}.')
+else:
+    print('Autenticación fallida. Usuario no reconocido.')
