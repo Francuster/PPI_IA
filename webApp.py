007ae45f21e69c47bfc22256b49bc6acf2c30a98
matizipi <matiassuarez265@gmail.com>
2024-04-07 13:18:30 -0300

    fix problema con CORS

--- webApp.py
+++ webApp.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from chatbot import generate_response
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
